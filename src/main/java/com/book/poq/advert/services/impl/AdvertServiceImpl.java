34afa6e9f1a054f7af7afd6a4afd88b453981c6d
hrebeniukov <hrebeniukov@netcracker.com>
2020-03-26 15:39:25 +0200

    AdvertPoq mongo

--- src/main/java/com/book/poq/advert/services/impl/AdvertServiceImpl.java
+++ src/main/java/com/book/poq/advert/services/impl/AdvertServiceImpl.java
@@ -1,6 +1,8 @@
 package com.book.poq.advert.services.impl;
 
 import com.book.poq.advert.entity.Advert;
+import com.book.poq.advert.entity.AdvertId;
+import com.book.poq.advert.repositories.AdvertIdRepository;
 import com.book.poq.advert.repositories.AdvertRepository;
 import com.book.poq.advert.services.AdvertService;
 import reactor.core.publisher.Flux;
@@ -9,14 +11,21 @@ import reactor.core.publisher.Mono;
 public class AdvertServiceImpl implements AdvertService {
 
     protected AdvertRepository advertRepository;
+    protected AdvertIdRepository advertIdRepository;
 
-    public AdvertServiceImpl(AdvertRepository advertRepository) {
+    public AdvertServiceImpl(AdvertRepository advertRepository, AdvertIdRepository advertIdRepository) {
         this.advertRepository = advertRepository;
+        this.advertIdRepository = advertIdRepository;
     }
 
     @Override
     public Mono<Advert> getById(int id) {
         return advertRepository.findById(id);
+    }
+
+    @Override
+    public Mono<AdvertId> getByName(String name) {
+        return advertIdRepository.findFirstByName(name);
     }
 
     @Override
